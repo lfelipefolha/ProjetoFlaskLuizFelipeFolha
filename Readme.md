c07b36d11d982538fdcf9aa6a45ce8d621b1ea52
Luiz Felipe Folha <lfelipefota@gmail.com>
2022-01-31 22:06:09 -0300

    func email add and requeirements and data

---
+++ Readme.md
@@ -0,0 +1,32 @@
+>#### **Requirements:**  
+>blinker==1.4  
+>certifi==2021.10.8  
+charset-normalizer==2.0.10    
+click==8.0.3  
+colorama==0.4.4  
+Flask==2.0.2  
+Flask-Mail==0.9.1  
+Flask-SQLAlchemy==2.5.1  
+greenlet==1.1.2  
+gunicorn==20.1.0  
+heroku==0.1.4  
+idna==3.3  
+itsdangerous==2.0.1  
+Jinja2==3.0.3  
+MarkupSafe==2.0.1  
+python-dateutil==1.5  
+requests==2.27.1  
+SQLAlchemy==1.4.31  
+urllib3==1.26.8  
+>Werkzeug==2.0.2  
+
+
+> #### FUNCIONALIDADES:
+>
+>1. CRUD completo de todos os dados
+>2. Enviar para o **email** informado a **tarefa**, (3.) o **prazo de entrega**(informação adicional implementada) e (4.)  uma **frase motivacional** escolhida aleatoriamente de uma lista de frases predefinida
+>
+
+> #### Ideias para os próximos passos da aplicação
+> 1. A partir do prazo de entrega, fazer a conexão com o **Google Calendaer** através do **GOOGLE CALENDAR API**, adicionando um lembrete para o usuário
+> 2.   Integrar também o **Maps Plataform** com o **Google Calendar**, adicionando assim o local de realização da tarefa 
--- app.py
+++ app.py
@@ -2,77 +2,89 @@ from flask import Flask , render_template , redirect , url_for , request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_mail import Mail, Message
-
-
-app = Flask ( __name__ )
-app.config [ 'SQLALCHEMY_DATABASE_URI' ] = 'sqlite:///testedb.db'
+import random
+app = Flask ( __name__ ) #inicia uma aplicação utilizando Flask
+app.config [ 'SQLALCHEMY_DATABASE_URI' ] = 'sqlite:///testedb.db' #configura a base de dados
 db = SQLAlchemy ( app )
 
+#nessa etapa é feita a configuração para enviar emails através da aplicação
 mail= Mail(app)
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'folhaflaskapp@gmail.com'
+app.config['MAIL_USERNAME'] = 'folhaflaskapp@gmail.com' #email utilizado para o propósito
 app.config['MAIL_PASSWORD'] = 'batata21'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
 
+#cria a base de dados da aplicação
 @app.before_first_request
 def create_table() :
     db.create_all ()
 
+#aqui é feita a modelagem do banco de dados
 class Todo ( db.Model ) :
     id = db.Column ( db.Integer , primary_key=True )
     content = db.Column ( db.String ( 200 ) , nullable=False )
     completed = db.Column ( db.Integer , default=0 )
     email = db.Column (db.String (200), nullable=False)
     date_created = db.Column ( db.DateTime , default=datetime.utcnow )
+    prazo = db.Column( db.String, nullable=False )
 
     def __repr__(self) :
         return '<Task %r>' % self.id
 
 
+#define a rota inicial e cria a função principal da app
 @app.route ( '/' , methods=[ 'POST' , 'GET' ] )
 def index() :
-
+    #faz a requisão dos dados do formulário
     if request.method == 'POST' :
         task_content = request.form [ 'content' ]
         task_email = request.form [ 'email' ]
-        new_task = Todo ( content=task_content, email=task_email )
+        prazo = request.form [ 'prazo' ]
+        new_task = Todo ( content=task_content, email=task_email, prazo = prazo )
 
+        #add os dados no banco de dados
         try :
             db.session.add ( new_task )
             db.session.commit ()
             return redirect ( '/' )
         except :
             return 'Problema ao add tarefa'
-
+    #devolve os dados já obtidos na tela principal
     else :
 
         dados = Todo.query.order_by ( Todo.date_created ).all ()
 
         return render_template ('index.html', dados = dados)
 
-
+#função para deletar os dados que foram add
 @app.route ( '/delete/<int:id>' )
 def delete(id) :
+    #solicita através do id de cada dado, caso não exista é retornado um código de erro
     task_to_delete = Todo.query.get_or_404 ( id )
 
+    #apaga os dados obtidos
     try :
         db.session.delete ( task_to_delete )
         db.session.commit ()
         return redirect ( '/' )
 
+    #retorna uma exceção caso ocorra algum erro
     except :
         return 'There was a problem deleting that task'
 
+#funcao responsável por atualizar os dados
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     task = Todo.query.get_or_404(id)
 
     if request.method == 'POST':
         task.content = request.form['content']
+        task.email = request.form [ 'email' ]
+        task.prazo = request.form [ 'prazo' ]
 
         try:
             db.session.commit()
@@ -82,19 +94,33 @@ def update(id):
     else:
         return render_template('update.html', task = task )
 
-
+#função utilizada para enviar os emails
 @app.route("/sendmail/<int:id>")
 def sendmail(id):
+        lista =["Se não houver vento, reme.",
+            "Não limite seus desafios. Desafie seus limites.",
+            "Minha missão na vida não é apenas sobreviver, mas prosperar.",
+            "O que você procura está procurando você.",
+            "Uma meta é um sonho com um prazo.",
+            "Tente mover o mundo - o primeiro passo será mover a si mesmo.",
+            "Disciplina é a ponte entre metas e realizações.",
+            "Sorte é o que acontece quando a preparação encontra a oportunidade.",
+            "Eu faço da dificuldade a minha motivação. A volta por cima vem na continuação."]
+        frase = random.choice(lista)
+        #através da id são obtidos os dados de tarefa e endereço de email
         task_to_send = Todo.query.filter_by ( id = id ).first ()
-        recipients = task_to_send.email
-        body = task_to_send.content
+        recipients = task_to_send.email #destinatário
+        body = (str(task_to_send.content) + "\nFrase do dia: " +  str(frase))  #tarefa
+
+        #aqui é realizada a definição do email a ser enviado
         msg = Message(subject="Projeto de organização de tarefas",
                       sender='folhaflaskapp@gmail.com',
                       recipients=[recipients], # replace with your email for testing
-                      body=body)
-        mail.send(msg)
-        return render_template('sendmail.html')
+                      body= str(body))
 
+        mail.send(msg) #função que envia o email
+
+        return render_template('sendmail.html')
 
 
 
